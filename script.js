2a1abf36866f12bbac8b1a49bf9039cfa8d7c01a
davi <davivalvassori123@gmail.com>
2024-04-04 15:06:01 -0300

    :aaaaaaaaa: inicial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+function togglemode(){
+
+ const html = document.body
+ html.classList.toggle('light')
+
+
+ //pegar a tag image
+ const img=document.querySelector("#profile img")
+ 
+ //substituir a imagem
+  
+ 
+ if(html.classList.contains('light')){
+//se tiver light mode add a imagem light
+img.setAttribute('src', './assets/gato.png')
+} else{
+//se tiver sem light mode manter a imagem normal
+img.setAttribute('src', './assets/picapausad.png')
+
+}
+
+
+
+
+
+}
