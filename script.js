23a8836f061b0e0219406f1b4a271d3e35d572e0
davi <davivalvassori123@gmail.com>
2024-04-10 18:26:33 -0300

    a

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function togglemode(){
 img.setAttribute('src', './assets/gato (cópia).png')
 } else{
 //se tiver sem light mode manter a imagem normal
-img.setAttribute('src', './assets/picapausad (cópia).png')
+img.setAttribute('src', './assets/picapausad .png')
 
 }
 
